12a1c9c35209f39b1a5acaf5d0d7b42fc54ff358
sambao407 <sambao407@gmail.com>
2017-06-24 22:16:04 +0200

    Diagral

--- src/main/java/fr/exia/game/App.java
+++ src/main/java/fr/exia/game/App.java
@@ -8,8 +8,8 @@ import view.ReadMap;
 public class App {
     public static void main( String[] args ) {
         System.out.println( "Hello World!" );
-        //GameFrame gameFrame = new GameFrame();
+        GameFrame gameFrame = new GameFrame();
         //ReadMap readMap = new ReadMap();
-        Connect connect = new Connect();
+        //Connect connect = new Connect();
     }
 }
